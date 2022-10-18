b6c826d84359011321172e70ac8da722e47d9126
Júlio Santos <hi@whoisjuliosantos.com>
2022-10-18 11:04:14 +0200

    Variable storageByteDeposit across networks

--- utils/karuraAndAcalaTransactionHelpers.js
+++ utils/karuraAndAcalaTransactionHelpers.js
@@ -1,22 +1,29 @@
+const hardhat = require("hardhat");
 const { calcEthereumTransactionParams } = require("@acala-network/eth-providers");
 
 async function txParams() {
-    const txFeePerGas = '199999946752';
-    const storageByteDeposit = '100000000000000';
-    const blockNumber = await ethers.provider.getBlockNumber();
+  let storageByteDeposit;
 
-    const ethParams = calcEthereumTransactionParams({
-      gasLimit: '31000000',
-      validUntil: (blockNumber + 100).toString(),
-      storageLimit: '64001',
-      txFeePerGas,
-      storageByteDeposit
-    });
+  if (hardhat.network.name === "mandala" || hardhat.network.name === "karura") {
+    storageByteDeposit = "100000000000000";
+  } else if (hardhat.network.name === "acala") {
+    storageByteDeposit = "300000000000000";
+  }
+  const txFeePerGas = "199999946752";
+  const blockNumber = await ethers.provider.getBlockNumber();
 
-    return {
-        gasPrice: ethParams.txGasPrice,
-        gasLimit: ethParams.txGasLimit
-    };
+  const ethParams = calcEthereumTransactionParams({
+    gasLimit: "31000000",
+    validUntil: (blockNumber + 100).toString(),
+    storageLimit: "64001",
+    txFeePerGas,
+    storageByteDeposit,
+  });
+
+  return {
+    gasPrice: ethParams.txGasPrice,
+    gasLimit: ethParams.txGasLimit,
+  };
 }
 
 module.exports = { txParams };
